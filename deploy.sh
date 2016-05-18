ffdd27b901cdd23c72dd39eb3c8a10d92d6d3e14
JonathanMontane <jon.montane@gmail.com>
2016-05-18 14:23:13 +0200

    fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-exporters=( $(ls ./src/exporters) )
+exporters=( $(ls ./src/serializers/paw) )
 
 for line in "${exporters[@]}"; do
 	if [ "$line" != "base-importer" ]
 	then
 		echo "building $line importer";
-		if [ -f "./src/exporters/$line/webpack.config.babel.js" ]
+		if [ -f "./src/serializers/paw/$line/webpack.config.babel.js" ]
 		then
-			BUILD_ENV=build ./node_modules/.bin/webpack --bail --display-error-details --config="./src/exporters/$line/webpack.config.babel.js"
+			BUILD_ENV=build ./node_modules/.bin/webpack --bail --display-error-details --config="./src/serializers/paw/$line/webpack.config.babel.js"
 		fi;
 	fi;
 done
