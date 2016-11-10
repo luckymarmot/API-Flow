18f57f4628303702ce1eb83930222625e8f3d164
JonathanMontane <jon.montane@gmail.com>
2016-11-10 18:43:01 +0100

    refactored runners

--- src/runners/flow-web.js
+++ src/runners/flow-web.js
@@ -1,126 +1,45 @@
 import SwaggerParser from '../parsers/swagger/Parser'
 import RAMLParser from '../parsers/raml/Parser'
-import PostmanParserV1 from '../parsers/postman/v1/Parser'
-import PostmanParserV2 from '../parsers/postman/v2/Parser'
+import PostmanParser from '../parsers/postman/Parser'
 import CurlParser from '../parsers/curl/Parser'
 
 import SwaggerSerializer from '../serializers/swagger/Serializer'
 import RAMLSerializer from '../serializers/raml/Serializer'
 import PostmanSerializer from '../serializers/postman/Serializer'
 import CurlSerializer from '../serializers/cURL/Serializer'
+import InternalSerializer from '../serializers/internal/Serializer'
 
-import ContextResolver from '../resolvers/ContextResolver'
 import BrowserEnvironment, {
     URLResolver
 } from '../models/environments/BrowserEnvironment'
 
-import Options from '../models/options/Options'
+import BaseFlow from './base-runner'
 
-export default class FlowBrowser {
-    detect(content) {
-        let parserMap = {
-            swagger: SwaggerParser,
-            raml: RAMLParser,
-            'postman-1': PostmanParserV1,
-            'postman-2': PostmanParserV2,
-            curl: CurlParser
-        }
-
-        let score = {}
-
-        let parsers = Object.keys(parserMap)
-        for (let parser of parsers) {
-            let _parser = new parserMap[parser]()
-            score[parser] = _parser.detect(content)
-        }
-
-        return score
+export default class FlowBrowser extends BaseFlow {
+    static parsers = {
+        swagger: SwaggerParser,
+        raml: RAMLParser,
+        postman: PostmanParser,
+        curl: CurlParser
     }
 
-    transform(input, callback, _opts) {
-        let parserMap = {
-            swagger: SwaggerParser,
-            raml: RAMLParser
-        }
+    static serializers = {
+        swagger: SwaggerSerializer,
+        raml: RAMLSerializer,
+        postman: PostmanSerializer,
+        curl: CurlSerializer,
+        __internal__: InternalSerializer
+    }
 
-        let serializerMap = {
-            swagger: SwaggerSerializer,
-            raml: RAMLSerializer,
-            postman: PostmanSerializer,
-            curl: CurlSerializer
-        }
+    constructor() {
+        super(BrowserEnvironment, URLResolver)
+    }
 
-        let opts = new Options(_opts)
+    getParsers() {
+        return FlowBrowser.parsers
+    }
 
-        let source = opts.getIn([ 'parser', 'name' ])
-        let target = opts.getIn([ 'serializer', 'name' ])
-        let base = opts.getIn([ 'resolver', 'base' ])
-
-        if (!parserMap[source]) {
-            throw new Error('unrecognized source format')
-        }
-
-        if (!serializerMap[target]) {
-            throw new Error('unrecognized target format')
-        }
-
-        let contentPromise
-        if (base === 'raw') {
-            contentPromise = new Promise((resolve) => {
-                return resolve(input)
-            })
-        }
-        else {
-            contentPromise = (new URLResolver()).resolve(input)
-        }
-
-        let parser = new parserMap[source]()
-        let serializer = new serializerMap[target]()
-        let environment = new BrowserEnvironment()
-        let resolver = new ContextResolver(environment)
-
-        return contentPromise.then((content) => {
-            let item = {
-                url: input,
-                content: content
-            }
-
-            let promise = parser.parse(item, opts.get('parser'))
-
-            if (typeof promise.then !== 'function') {
-                let value = promise
-                promise = new Promise((resolve) => {
-                    resolve(value)
-                })
-            }
-
-            return promise.then(context => {
-                return resolver.resolveAll(
-                    parser.item,
-                    context,
-                    opts.get('resolver')
-                ).then(_context => {
-                    try {
-                        let final = serializer
-                            .serialize(
-                                _context,
-                                opts.get('serializer')
-                            )
-                        callback(null, final)
-                        return final
-                    }
-                    catch (e) {
-                        callback(e.stack, null)
-                        throw e
-                    }
-                }).catch(error => {
-                    callback(error.stack)
-                })
-            }, error => {
-                callback(error.stack)
-            }).catch(err => {
-                callback(err.stack)
-            })
-        })
+    getSerializers() {
+        return FlowBrowser.serializers
     }
 }
