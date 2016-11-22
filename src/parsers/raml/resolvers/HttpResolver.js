273f0e645a05a037953c4f595039130270313124
JonathanMontane <jon.montane@gmail.com>
2016-11-22 11:47:36 +0100

    updated raml parser to match new context model

---
+++ src/parsers/raml/resolvers/HttpResolver.js
@@ -0,0 +1,30 @@
+import path from 'path-browserify'
+
+export default class ShimmingHttpResolver {
+    constructor(items) {
+        this.baseItem = ''
+        this.items = items || []
+    }
+
+    readFileAsync(filePath) {
+        for (let item of this.items) {
+            if (filePath.indexOf(
+                path.join(item.file.path, item.file.name)
+            ) === 0) {
+                return new Promise((resolve) => {
+                    resolve(item.content)
+                })
+            }
+        }
+        return new Promise((resolve) => {
+            resolve(
+                '::fileRef::' +
+                path.relative(path.dirname(this.baseItem), filePath)
+            )
+        })
+    }
+
+    setBaseItem(item) {
+        this.baseItem = item.getPath()
+    }
+}
---
+++ src/parsers/raml/resolvers/__tests__/FileResolver-test.js
@@ -0,0 +1,61 @@
+import { UnitTest, registerTest } from '../../../../utils/TestUtils'
+
+import ShimmingFileReader from '../FileResolver'
+
+@registerTest
+export class TestFileReader extends UnitTest {
+    testConstructor() {
+        let items = [ 1, 2, 3, 'test' ]
+
+        let reader = new ShimmingFileReader(items)
+        this.assertEqual(reader.items, items)
+    }
+
+    testReadFileAsyncWithSimpleFile() {
+        const items = [
+            {
+                content: 'Lorem Ipsum dolor sic amet',
+                file: {
+                    path: '/some/path',
+                    name: 'simpleFile'
+                }
+            }
+        ]
+
+        let reader = new ShimmingFileReader(items)
+
+        reader.readFileAsync('/some/path/simpleFile').then(
+            data => {
+                this.assertEqual(items[0].content, data)
+            }
+        ).catch(
+            () => {
+                this.assertTrue(false)
+            }
+        )
+    }
+
+    testReadFileAsyncWithMissingFile() {
+        const items = [
+            {
+                content: 'Lorem Ipsum dolor sic amet',
+                file: {
+                    path: '/some/path',
+                    name: 'simpleFile'
+                }
+            }
+        ]
+
+        let reader = new ShimmingFileReader(items)
+
+        reader.readFileAsync('/some/path/missingFile').then(
+            data => {
+                this.assertEqual(data, '::fileRef::/some/path/missingFile')
+            }
+        ).catch(
+            () => {
+                this.assertTrue(false)
+            }
+        )
+    }
+}
