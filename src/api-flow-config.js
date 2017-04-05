20581251d582458c020a155e595cfcd284444c11
JonathanMontane <jon.montane@gmail.com>
2017-04-05 17:20:54 +0200

    added postman collection v2 loader and parser to api-flow-config

--- src/api-flow-config.js
+++ src/api-flow-config.js
@@ -3,10 +3,12 @@ import Environment from './environments/node/Environment'
 import SwaggerLoader from './loaders/swagger/Loader'
 import RAMLLoader from './loaders/raml/Loader'
 import InternalLoader from './loaders/internal/Loader'
+import PostmanCollectionV2Loader from './loaders/postman/v2.0/Loader'
 
 import SwaggerV2Parser from './parsers/swagger/v2.0/Parser'
 import RAMLV1Parser from './parsers/raml/v1.0/Parser'
 import InternalParser from './parsers/internal/Parser'
+import PostmanCollectionV2Parser from './parsers/postman/v2.0/Parser'
 
 import SwaggerV2Serializer from './serializers/swagger/v2.0/Serializer'
 import RAMLV1Serializer from './serializers/raml/v1.0/Serializer'
@@ -16,13 +18,15 @@ import PostmanV2Serializer from './serializers/postman/v2.0/Serializer'
 export const loaders = [
   SwaggerLoader,
   RAMLLoader,
-  InternalLoader
+  InternalLoader,
+  PostmanCollectionV2Loader
 ]
 
 export const parsers = [
   SwaggerV2Parser,
   RAMLV1Parser,
-  InternalParser
+  InternalParser,
+  PostmanCollectionV2Parser
 ]
 
 export const serializers = [
