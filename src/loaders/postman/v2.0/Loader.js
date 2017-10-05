c46ad2f4244c8641c47c5d57e7135a42f72ed8d3
JonathanMontane <jon.montane@gmail.com>
2017-10-06 00:06:58 +0200

    linting

--- src/loaders/postman/v2.0/Loader.js
+++ src/loaders/postman/v2.0/Loader.js
@@ -87,7 +87,7 @@ methods.isParsable = (content) => {
 methods.resolve = (options, uri, { $ref = '' } = {}) => {
   const uriToLoad = resolve(uri, $ref)
   const protocol = parse(uriToLoad).protocol
-  if (protocol && protocol.substr(0,4) === 'http') {
+  if (protocol && protocol.substr(0, 4) === 'http') {
     return options.httpResolver.resolve(uriToLoad.split('#')[0])
   }
 
